fd6a0acb2c011044d0916cb1b304c3955db1b10d
Przemek Rudzki <przemek@kritik.io>
2020-12-22 09:25:11 -0500

    add chakra buttons, styling

--- src/App.js
+++ src/App.js
@@ -2,35 +2,36 @@ import './App.css'
 import { Add } from './redux/calc/actions'
 import { connect } from 'react-redux'
 import { getCurrentValue, resetValue } from './redux/calc/actions'
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App(props) {
   function handleClick() {
     console.log('clicked')
-    Add(Math.floor(Math.random()*20))
+    Add(Math.floor(Math.random() * 20))
   }
-  function handleReset(){
+  function handleReset() {
     resetValue()
   }
   return (
     <div className="App">
-      <h1>
-        CALCULATOR WITH REACT
-      </h1>
-      <h2>
-        {props.currentValue}
-      </h2>
-      <button onClick={handleClick}>Click</button>
-      <button onClick={handleReset}>Reset</button>
+      <h1 style={{fontSize: '40px', fontWeight: 'bold'}}>CALCULATOR</h1>
+      <div style={{margin: '1rem'}}>{props.currentValue}</div>
+      <ButtonGroup>
+        <Button onClick={handleClick} colorScheme="blue">
+          Click
+        </Button>
+        <Button onClick={handleReset} variant="outline" colorScheme="blue">
+          Reset
+        </Button>
+      </ButtonGroup>
     </div>
-  );
+  )
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    currentValue: getCurrentValue(state)
+    currentValue: getCurrentValue(state),
   }
-  
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
